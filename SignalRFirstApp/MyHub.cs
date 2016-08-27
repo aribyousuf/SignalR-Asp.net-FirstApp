2f5af33420987b6c370944223f59503000b72df0
Arib Yousuf <Arib Yousuf>
2016-08-27 09:18:51 +0500

    App Completed

---
+++ SignalRFirstApp/MyHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRFirstApp
+{
+    public class MyHub : Hub
+    {
+        public void Work(string task)
+        {
+            Clients.All.Work(task);
+        }
+    }
+}
---
+++ SignalRFirstApp/Scripts/SignalR.js
@@ -0,0 +1,11 @@
+﻿$.connection.hub.start()
+    .done(function () {
+        console.log("It worked")
+        $.connection.myHub.server.work("Connected !");
+    })
+    .fail(function () {
+        alert("Error")
+    });
+$.connection.myHub.client.work = function (message) {
+    $("#main").append(message + "<br />")
+}
